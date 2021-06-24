88b966fffa334aab9213781d5118b1254b20f3fa
My <abc@cab.com>
2021-06-24 18:29:33 +0300

    add file input

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -1,22 +1,40 @@
 // palindrome.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <map>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
+namespace fs = filesystem;
 
 int main()
 {
     map<char, uint32_t> letters;
     int n;
-    string str;
-
-    cin >> n >> str;
+    string str = "";
+    
+ 
+    n = fs::file_size("E:\\Projects\\Pract\\test11.txt");
+    char* masCh = new char[n + 1];
+    ifstream myin("E:\\Projects\\Pract\\test11.txt");
+    if (myin.is_open())
+    {
+        myin >> masCh;
+    }
+    
+    
+    masCh[n] = 0;
     for (int i = 0; i < n; i++)
-        letters.find(str[i]) == letters.end() ? letters[str[i]] = 1 : letters[str[i]] += 1;
-
-    str = "";
+    {
+        char ch = masCh[i];
+ 
+        letters.find(ch) == letters.end() ? letters[ch] = 1 : letters[ch] += 1;
+        if (letters.find(-51) != letters.end())
+        {
+            cout << i << masCh[i];
+        }
+    }
 
     map<char, uint32_t>::iterator firstodd = letters.end();
     for (map<char, uint32_t>::iterator i = letters.begin(); i != letters.end(); i++)
@@ -53,5 +71,7 @@ int main()
             str += str[i];
     }
 
-    cout << str << '\n';
+    ofstream out("E:\\Projects\\Pract\\out.txt");
+    out << str;
+    out.close();
 }
