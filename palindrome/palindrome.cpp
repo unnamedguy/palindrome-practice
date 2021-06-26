9dfd2476915f2fc3657ea542f28dd31be76934cb
unnamedguy <unnamedjguy@icloud.com>
2021-06-26 16:16:14 +0300

    Add input word check

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -8,14 +8,25 @@
 using namespace std;
 namespace fs = filesystem;
 
+bool checkWord(const char* word, int n);
+
+
 int main()
 {
     map<char, uint32_t> letters;
     int n;
     string str = "";
+
     
- 
     n = fs::file_size("E:\\Projects\\Pract\\test11.txt");
+
+    if (n == 0 && n > 100000)
+    {
+        ofstream out("E:\\Projects\\Pract\\out.txt", ios::trunc);
+        out.close();
+        return 0;
+    }
+
     char* masCh = new char[n + 1];
     ifstream myin("E:\\Projects\\Pract\\test11.txt");
     if (myin.is_open())
@@ -25,15 +36,23 @@ int main()
     
     
     masCh[n] = 0;
+
+    if (!checkWord(masCh, n))
+    {
+        ofstream out("E:\\Projects\\Pract\\out.txt", ios::trunc);
+        out.close();
+        return 0;
+    }
+
     for (int i = 0; i < n; i++)
     {
         char ch = masCh[i];
  
         letters.find(ch) == letters.end() ? letters[ch] = 1 : letters[ch] += 1;
-        if (letters.find(-51) != letters.end())
-        {
-            cout << i << masCh[i];
-        }
+        //if (letters.find(-51) != letters.end())
+        //{
+        //    cout << i << masCh[i];
+        //}
     }
 
     map<char, uint32_t>::iterator firstodd = letters.end();
@@ -74,4 +93,13 @@ int main()
     ofstream out("E:\\Projects\\Pract\\out.txt");
     out << str;
     out.close();
+}
+
+bool checkWord(const char* word, int n)
+{
+    for (int i = 0; i < n; i++)
+        if (word[i] < 'A' || word[i] > 'Z')
+            return false;
+
+    return true;
 }
