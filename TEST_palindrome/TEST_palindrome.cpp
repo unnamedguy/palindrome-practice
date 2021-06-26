6fee6a3ae9a15fee0c5ff957c3b24c7ab055669d
emptyIvan <ivanovvany2295@gmail.com>
2021-06-26 16:36:10 +0300

    fix old and add new tests

--- TEST_palindrome/TEST_palindrome.cpp
+++ TEST_palindrome/TEST_palindrome.cpp
@@ -15,19 +15,7 @@ bool test8();
 bool test9();
 bool test10();
 
-int testTask()
-{
-	std::vector<bool (*)()>functptr = { test1, test2, test3, test4, test5 };//, test6, test7, test8, test9, test10 };
-	for (size_t i = 0; i < functptr.size(); i++)
-	{
-		if (!(*functptr[i])())
-		{
-			return i + 1;
-		}		
-	}
-	return 0;
-}
-
+int testTask();
 
 int main() 
 {
@@ -41,6 +29,21 @@ int main()
 		std::cout <<"Test " << N << " failed";
 	}
 }
+
+
+int testTask()
+{
+	std::vector<bool (*)()>functptr = { test1, test2, test3, test4, test5, test6, test7, test8,  test9, test10 }; 
+	for (size_t i = 0; i < functptr.size(); i++)
+	{
+		if (!(*functptr[i])())
+		{
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
 
 bool test1() //нечет - общее кол-во символов, нечет - конкретной буквы
 {
@@ -67,6 +70,32 @@ bool test5() //
 	return streamFile("ASDASDASDASDASDZSDZSDZSDZSDZSDZSFFEFEFEFEFQEFSEFSDFGDFHGDFHHD") == "AADDDDDDDEEEFFFFFGHSSSSSSZZZAZZZSSSSSSHGFFFFFEEEDDDDDDDAA";
 }
 
+bool test6() //проверка пустого символа
+{
+	return  streamFile("") == "";
+
+}
+
+bool test7() //проверка недопустимых символов (кириллицы)
+{
+	return streamFile("Проверка кириллицы") == "";
+}
+
+bool test8() //проверка недопустимых символов
+{
+	return streamFile("!~@#$%^^*(&%_)__+") == "";
+}
+
+bool test9() //проверка нижнего регистра
+{
+	return streamFile("aassddffgg") == "";
+}
+
+bool test10() //проверка палиндрома из 1 символа
+{
+	return streamFile("A") == "A";
+}
+
 std::string streamFile(std::string str)
 {
 	std::ofstream out("..\\test11.txt");
@@ -74,6 +103,7 @@ std::string streamFile(std::string str)
 	out << str;
 	out.close();
 	system("..\\x64\\Debug\\palindrome.exe");
+	str = "";
 	std::ifstream in("..\\out.txt");
 	in >> str;
 	in.close();
